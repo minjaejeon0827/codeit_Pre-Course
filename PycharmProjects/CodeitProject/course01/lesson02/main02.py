92f89db1621c1fef7ae554ed52fa8fdba22f04c0
DESKTOP-1D2MQJL\NT371B5A <minjaejeon0827@gmail.com>
2026-01-26 16:39:54 +0900

    main.py - 프로그래밍 시작하기 in Python -> 2. 프로그래밍 기본 개념 -> 02. 자료형 개요 커밋 완료

---
+++ PycharmProjects/CodeitProject/course01/lesson02/main02.py
@@ -0,0 +1,12 @@
+# 02. 자료형 개요
+# 2와 2.0 사이의 값 비교를 하기 위해 정수 2 -> 실수 2.0 으로 변환 후 값 비교 처리
+print(2 != 2.0) # 정수형(Integer)과 소수형(Floating Point) 값 비교
+
+# 데이터 타입(Data Type) 비교 하기 위해 type() 사용.
+# 참고 URL - https://chatgpt.com/c/6977176a-5f98-8321-ba5e-03864328ddbd
+print(type(2) != type(2.0))   # 정수형(Integer)과 소수형(Floating Point) 데이터 타입 비교
+print("Hello" + "World")   # 문자열 덧셈 연산(+)
+print((2 != 2.0) and (2 != "2"))   # 정수형(Integer), 소수형(Floating Point), 문자열(String) 값 비교
+print((type(2) != type(2.0)) and (type(2) != type("2")))   # 정수형(Integer), 소수형(Floating Point), 문자열(String) 데이터 타입 비교
+print(7 > 3)   # 불린(Boolean) 결과값 참(True)
+print(7 < 3)   # 불린(Boolean) 결과값 거짓(False)
