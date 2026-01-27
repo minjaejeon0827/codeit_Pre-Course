fac517915a08c505ce505ee5af0d0f1d03747b43
DESKTOP-1D2MQJL\NT371B5A <minjaejeon0827@gmail.com>
2026-01-27 19:20:57 +0900

    main08.py - 프로그래밍 시작하기 in Python -> 2. 프로그래밍 기본 개념 -> 08. 파라미터 커밋

---
+++ PycharmProjects/CodeitProject/course01/lesson02/main08.py
@@ -0,0 +1,26 @@
+# 08. 파라미터
+# 파라미터를 매개변수라고도 한다.
+# 파라미터는 함수를 정의할 때 쓰게 되는 변수를 말하는 별칭이라고 생각하면 된다.
+
+# 함수 매개변수와 인수
+# 참고 URL - https://wikidocs.net/24#_4
+
+def hello(name=""):   # def 기호 사용하여 구현한 함수 정의의 첫줄을 함수의 '헤더'라고 부른다.
+    """
+    Description: [테스트] 사용자 정의 함수
+
+    Parameters: name - 이름 (초기값 공백("") 설정)
+
+    Returns: None
+    """
+
+    # 해당 hello 함수는 아래 2 줄의 명령 저장 및 실행
+    print("Hello!")
+    if ("" != name): print(name)   # 변수 name에 저장된 값이 공백("")이 아닌 경우
+    print("Welcome to Codeit!")
+
+# hello 함수 2번 호출
+hello("Chris")  # hello 함수 name 파라미터로 문자열 "Chris" 인자값 전달
+hello("Michael")  # hello 함수 name 파라미터로 문자열 "Michael" 인자값 전달
+hello("")  # hello 함수 name 파라미터로 공백("") 인자값 전달
+hello()   # hello 함수 name 파라미터로 인자값 전달 안함.
