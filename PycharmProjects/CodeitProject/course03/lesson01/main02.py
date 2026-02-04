2bd282711c915509f82d1265dd46857b6c6fdd01
DESKTOP-1D2MQJL\NT371B5A <minjaejeon0827@gmail.com>
2026-02-04 12:15:33 +0900

    course02 -> lesson04 - main02.py - 프로그래밍과 데이터 in Python -> 1. 리스트  -> 02. 리스트 함수 커밋

---
+++ PycharmProjects/CodeitProject/course03/lesson01/main02.py
@@ -0,0 +1,30 @@
+# 02. 리스트 함수
+
+# 참고
+# 리스트에 사용할 수 있는 함수
+# len 함수 - 리스트의 요소가 몇개인지(길이가 몇인지) 리턴 (길이 - length 줄임말)
+# append 함수 - 리스트의 가장 오른쪽에 새로운 요소(값) 추가 ('추가 연산'이라 부른다.)
+# del 함수 - 리스트의 특정 '인덱스(index)' 요소(값) 제거 (삭제 - delete 줄임말)
+#           그리고 삭제된 요소의 오른쪽에 있던 값들은 모두 하나씩 앞으로 당겨진다. (기존) 인덱스 -> (변경) 인덱스 - 1
+# insert 함수 - 리스트의 특정 '인덱스(index)' 요소(값) 삽입(추가) ('삽입 연산'이라 부른다.)
+# 참고 URL - https://stackoverflow.com/questions/53932704/why-doesnt-list-insert-with-negative-index-insert-the-item-at-the-end
+
+numbers = []  # 요소가 없고 비어있는 numbers 리스트
+
+# len 함수
+print(len(numbers))  # 해당 numbers 리스트의 요소가 몇개인지(길이가 몇인지) 출력
+
+# append 함수
+numbers.append(5)  # numbers 리스트 가장 오른쪽에 새로운 요소 "5" 추가
+numbers.append(8)  # numbers 리스트 가장 오른쪽에 새로운 요소 "8" 추가
+print(numbers)
+print(len(numbers))  # 해당 numbers 리스트의 요소가 몇개인지(길이가 몇인지) 출력
+
+# del 함수
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+# del numbers[3]  # numbers 리스트 인덱스 3 요소 "7" 삭제
+print(numbers)
+
+# insert 함수
+numbers.insert(4, 37)  # numbers 리스트 인덱스 4 요소 "37" 삽입(추가)
+print(numbers)
